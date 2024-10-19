080518aa1a3e40785a0c81680286872c1bb9cca6
13142579 <fs568966@gmail.com>
2024-10-18 21:25:43 -0300

    colocando os arquivos iniciais da minha aplicação

---
+++ Eventos/src/main/java/ifrn/pi/Eventos/Controllers/IndexController.java
@@ -0,0 +1,17 @@
+package ifrn.pi.Eventos.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Chamou o método index");
+		return "home";
+		
+		
+	}
+
+}
